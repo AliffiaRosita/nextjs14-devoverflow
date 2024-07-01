a726cb9fd05c2d4cde539d2a40499978f374debe
Nur Hakim <nur10hkm@gmail.com>
2024-07-01 16:59:29 +0800

    Feat: change live video call list ui

--- app/(stream)/call/[id]/components/VideoCallList.tsx
+++ app/(stream)/call/[id]/components/VideoCallList.tsx
@@ -1,9 +1,15 @@
 import { Call } from "@stream-io/video-react-sdk";
 import { useMemo, useCallback } from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-
-import VideoCallCard from "./VideoCallCard";
+import { ChevronDown } from "lucide-react";
 
 interface VideoCallListProps {
   liveCalls: Call[] | undefined;
@@ -20,7 +26,7 @@ const VideoCallList = ({
     setIsShowCallRoom(true);
   }, [setIsShowCallRoom]);
 
-  const videoCallCards = useMemo(() => {
+  const videoCallList = useMemo(() => {
     if (!liveCalls || liveCalls.length === 0) {
       return (
         <h2 className="text-dark300_light700 text-center">
@@ -28,30 +34,56 @@ const VideoCallList = ({
         </h2>
       );
     }
+    return (
+      <>
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="flex-center flex w-full cursor-pointer items-center rounded-md bg-red-500 px-4 py-2.5 text-sm text-white">
+              Join Video Call
+              <ChevronDown size={15} className="ml-2" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-500">
+            {liveCalls.map((call: Call, index: number) => {
+              const createdBy = call?.state?.createdBy?.name || "";
+              const startAt = call.state?.startsAt?.toLocaleString() || "";
 
-    return liveCalls.map((call: Call) => (
-      <VideoCallCard
-        key={call.id}
-        title={call.state?.custom?.description || "Video Call Invitation"}
-        createdBy={call.state?.createdBy}
-        date={call.state?.startsAt?.toLocaleString() || "-"}
-        onClick={() => {
-          setCallRoomId(call.id);
-          setIsShowCallRoom(true);
-        }}
-      />
-    ));
+              const option = `${createdBy} - ${startAt}`;
+              return (
+                <div key={index}>
+                  <DropdownMenuItem
+                    className="cursor-pointer capitalize"
+                    onClick={() => {
+                      setCallRoomId(call.id);
+                      setIsShowCallRoom(true);
+                    }}
+                  >
+                    {option}
+                  </DropdownMenuItem>
+                  {index !== liveCalls.length - 1 && (
+                    <DropdownMenuSeparator className="border-dark-500" />
+                  )}
+                </div>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        or
+      </>
+    );
   }, [liveCalls, setCallRoomId, setIsShowCallRoom]);
 
   return (
-    <div className="flex-center flex flex-col gap-2">
-      {videoCallCards}
-      <Button
-        className="primary-gradient mt-2.5 rounded-md px-4 py-2.5 text-white"
-        onClick={handleStartNewCall}
-      >
-        Start A New Call
-      </Button>
+    <div className="flex-center flex size-full items-center">
+      <div className="flex-center flex flex-col gap-2">
+        {videoCallList}
+        <Button
+          className="primary-gradient rounded-md px-4 py-2.5 text-white"
+          onClick={handleStartNewCall}
+        >
+          Start A New Call
+        </Button>
+      </div>
     </div>
   );
 };

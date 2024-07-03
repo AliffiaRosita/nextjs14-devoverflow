2c2c41c23ea6a0247157b1479b51da54eab34244
AliffiaRosita <aliffia.rosita@gmail.com>
2024-07-03 20:00:14 +0800

    remove null cond

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -9,180 +9,179 @@ import { getFormattedNumber, getTimestamp } from "@/lib/utils";
 
 import SocialShare from "../shared/SocialShare";
 import IconButton from "../shared/IconButton";
+import { faSkype, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import {
-    faSkype,
-    faWhatsapp,
-} from "@fortawesome/free-brands-svg-icons";
-import {
-  faVideo,
-//   faPhoneAlt,
-  faVideoCamera,
-  faMessage,
+	faVideo,
+	//   faPhoneAlt,
+	faVideoCamera,
+	faMessage,
 } from "@fortawesome/free-solid-svg-icons";
 import { QuestionProps } from "@/types";
 import { useRouter } from "next/navigation";
 
 const QuestionCard = ({
-    _id,
-    title,
-    skills,
-    author,
-    upvotes,
-    views,
-    answers,
-    createdAt,
-    clerkId,
+	_id,
+	title,
+	skills,
+	author,
+	upvotes,
+	views,
+	answers,
+	createdAt,
+	clerkId,
 }: QuestionProps) => {
-    const isUserAuthor = clerkId && clerkId === author.clerkId;
+	const isUserAuthor = clerkId && clerkId === author.clerkId;
 
-    const router = useRouter();
+	const router = useRouter();
 
-    return (
-        <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-            <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-                <div>
-                    <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-                        {getTimestamp(createdAt)}
-                    </span>
-                    <Link href={`/question/${_id}`}>
-                        <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-2 flex-1">
-                            {title}
-                        </h3>
-                    </Link>
-                </div>
+	return (
+		<div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+			<div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+				<div>
+					<span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+						{getTimestamp(createdAt)}
+					</span>
+					<Link href={`/question/${_id}`}>
+						<h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-2 flex-1">
+							{title}
+						</h3>
+					</Link>
+				</div>
 
-                <SignedIn>
-                    {isUserAuthor && (
-                        <EditDeleteAction
-                            type="Question"
-                            itemId={JSON.stringify(_id)}
-                        />
-                    )}
-                </SignedIn>
-            </div>
+				<SignedIn>
+					{isUserAuthor && (
+						<EditDeleteAction
+							type="Question"
+							itemId={JSON.stringify(_id)}
+						/>
+					)}
+				</SignedIn>
+			</div>
 
-            <div className="mt-3.5 flex flex-wrap gap-2">
-                {skills.map((skill) => (
-                    <RenderTag
-                        key={skill._id}
-                        _id={skill._id}
-                        name={skill.name}
-                    />
-                ))}
-            </div>
+			<div className="mt-3.5 flex flex-wrap gap-2">
+				{skills.map((skill) => (
+					<RenderTag
+						key={skill._id}
+						_id={skill._id}
+						name={skill.name}
+					/>
+				))}
+			</div>
 
-            <div className=" mt-6 w-full flex-col flex-wrap ">
-                <div className="mt-3 flex justify-between gap-3 max-sm:flex-wrap max-sm:justify-start">
-                    <Metric
-                        imgUrl={author.picture}
-                        alt="user"
-                        value={author.name}
-                        title={` • asked ${getTimestamp(createdAt)}`}
-                        href={`/profile/${author._id}`}
-                        isAuthor
-                        textStyles="body-medium text-dark400_light700"
-                    />
-                    <div className="flex gap-3">       
-                        <IconButton
-                            onClick={() => {
-                                router.push(`/call/${_id}`);
-                            }}
-                            type="button"
-                            color={"blue"}
-                            icon={faVideoCamera}
-                            text={"Video Call"}
-                        />
-                        {/* <IconButton
+			<div className=" mt-6 w-full flex-col flex-wrap ">
+				<div className="mt-3 flex justify-between gap-3 max-sm:flex-wrap max-sm:justify-start">
+					<Metric
+						imgUrl={author.picture}
+						alt="user"
+						value={author.name}
+						title={` • asked ${getTimestamp(createdAt)}`}
+						href={`/profile/${author._id}`}
+						isAuthor
+						textStyles="body-medium text-dark400_light700"
+					/>
+					<div className="flex gap-3">
+						<IconButton
+							onClick={() => {
+								router.push(`/call/${_id}`);
+							}}
+							type="button"
+							color={"blue"}
+							icon={faVideoCamera}
+							text={"Video Call"}
+						/>
+						{/* <IconButton
                             onClick={() => {}}
                             type="button"
                             color={"green"}
                             icon={faPhoneAlt}
                             text={"Voice Call"}
                         /> */}
-                        <IconButton
-                            onClick={() => {
-                                router.push(`/message${!isUserAuthor ? `?userId=${author.clerkId}` : '' }`);
-                            }}
-                            type="button"
-                            color={"red"}
-                            icon={faMessage}
-                            text={"Message"}
-                        />
-                    </div>
-                </div>
+						<IconButton
+							onClick={() => {
+								router.push(
+									`/message${!isUserAuthor ? `?userId=${author.clerkId}` : ""}`
+								);
+							}}
+							type="button"
+							color={"red"}
+							icon={faMessage}
+							text={"Message"}
+						/>
+					</div>
+				</div>
 
-                <div className="mt-3 flex items-center justify-between gap-3 max-sm:flex-wrap max-sm:justify-start">
-                    <div className=" flex gap-3">
-                        <Metric
-                            imgUrl="/assets/icons/like.svg"
-                            alt="Upvotes"
-                            value={getFormattedNumber(upvotes.length)}
-                            title=" Votes"
-                            textStyles="small-medium text-dark400_light800"
-                        />
-                        <Metric
-                            imgUrl="/assets/icons/message.svg"
-                            alt="Message"
-                            value={getFormattedNumber(answers.length)}
-                            title=" Answers"
-                            textStyles="small-medium text-dark400_light800"
-                        />
-                        <Metric
-                            imgUrl="/assets/icons/eye.svg"
-                            alt="Eye"
-                            value={getFormattedNumber(views)}
-                            title=" Views"
-                            textStyles="small-medium text-dark400_light800"
-                        />
-                        <SocialShare id={_id} />
-                    </div>
+				<div className="mt-3 flex items-center justify-between gap-3 max-sm:flex-wrap max-sm:justify-start">
+					<div className=" flex gap-3">
+						<Metric
+							imgUrl="/assets/icons/like.svg"
+							alt="Upvotes"
+							value={getFormattedNumber(upvotes.length)}
+							title=" Votes"
+							textStyles="small-medium text-dark400_light800"
+						/>
+						<Metric
+							imgUrl="/assets/icons/message.svg"
+							alt="Message"
+							value={getFormattedNumber(answers.length)}
+							title=" Answers"
+							textStyles="small-medium text-dark400_light800"
+						/>
+						<Metric
+							imgUrl="/assets/icons/eye.svg"
+							alt="Eye"
+							value={getFormattedNumber(views)}
+							title=" Views"
+							textStyles="small-medium text-dark400_light800"
+						/>
+						<SocialShare id={_id} />
+					</div>
 
-                    <div className="flex gap-3">
-                        {author?.whatsapp && (
-                            <IconButton
-                                link={`https://wa.me/${author?.whatsapp}`}
-                                color={"green"}
-                                icon={faWhatsapp}
-                                text={"Whatsapp"}
-                            />
-                        )}
-                        {author?.googleMeet && (
-                            <IconButton
-                                image={"/assets/images/gMeet.png"}
-                                color={"lightBlue"}
-                                text={"Google Meet"}
-                                link={`${author?.googleMeet}`}
-                            />
-                        )}
-                        {author?.zoom && (
-                            <IconButton
-                                link={`${author?.zoom}`}
-                                color={"blue"}
-                                icon={faVideo}
-                                text={"Zoom"}
-                            />
-                        )}
-                        {author?.teams && (
-                            <IconButton
-                                image={"/assets/images/teams.png"}
-                                color={"purple"}
-                                text={"Teams"}
-                                link={`${author?.teams}`}
-                            />
-                        )}
-                        {author?.skype && (
-                            <IconButton
-                                link={`${author?.skype}`}
-                                color={"lightBlue"}
-                                icon={faSkype}
-                                text={"Skype"}
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+					<div className="flex gap-3">
+						{author?.whatsapp && (
+							<IconButton
+								link={`https://wa.me/${author?.whatsapp}`}
+								color={"green"}
+								icon={faWhatsapp}
+								text={"Whatsapp"}
+							/>
+						)}
+						{author?.googleMeet && (
+							<IconButton
+								image={"/assets/images/gMeet.png"}
+								color={"lightBlue"}
+								text={"Google Meet"}
+								link={`${author?.googleMeet}`}
+							/>
+						)}
+						{author?.zoom && (
+							<IconButton
+								link={`${author?.zoom}`}
+								color={"blue"}
+								icon={faVideo}
+								text={"Zoom"}
+							/>
+						)}
+						{author?.teams && (
+							<IconButton
+								image={"/assets/images/teams.png"}
+								color={"purple"}
+								text={"Teams"}
+								link={`${author?.teams}`}
+							/>
+						)}
+						{author?.skype && (
+							<IconButton
+								link={`${author?.skype}`}
+								color={"lightBlue"}
+								icon={faSkype}
+								text={"Skype"}
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default QuestionCard;
--- components/shared/QuestionsContainer.tsx
+++ components/shared/QuestionsContainer.tsx
@@ -6,70 +6,66 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import NoResult from "@/components/shared/NoResult";
 
 const QuestionsContainer = ({
-    questions,
-    clerkId,
-    type,
+	questions,
+	clerkId,
+	type,
 }: {
-    questions: QuestionProps[];
-    clerkId: string | null | undefined;
-    type?: "collection" | "skill";
+	questions: QuestionProps[];
+	clerkId: string | null | undefined;
+	type?: "collection" | "skill";
 }) => {
-    if (!clerkId || !questions?.length) {
-        return null;
-    }
+	const renderNoResult = () => {
+		if (type === "collection") {
+			return (
+				<NoResult
+					title="No Saved Questions Found"
+					description="It appears that there are no saved questions in your collection at the moment 😔. Start exploring and saving questions that pique your interest 🌟"
+					link="/home"
+					linkTitle="Explore Questions"
+				/>
+			);
+		}
 
-    const renderNoResult = () => {
-        if (type === "collection") {
-            return (
-                <NoResult
-                    title="No Saved Questions Found"
-                    description="It appears that there are no saved questions in your collection at the moment 😔. Start exploring and saving questions that pique your interest 🌟"
-                    link="/home"
-                    linkTitle="Explore Questions"
-                />
-            );
-        }
+		if (type === "skill") {
+			return (
+				<NoResult
+					title="No Tag Questions Found"
+					description="It appears that there are no saved questions in your collection at the moment 😔.Start exploring and saving questions that pique your interest 🌟"
+					link="/home"
+					linkTitle="Explore Questions"
+				/>
+			);
+		}
 
-        if (type === "skill") {
-            return (
-                <NoResult
-                    title="No Tag Questions Found"
-                    description="It appears that there are no saved questions in your collection at the moment 😔.Start exploring and saving questions that pique your interest 🌟"
-                    link="/home"
-                    linkTitle="Explore Questions"
-                />
-            );
-        }
-
-        return (
-            <NoResult
-                title="No Questions Found"
-                description="Be the first to break the silence! 🚀 Post a Problem and kickstart the discussion. Your query could be the next big thing others learn from. Get involved! 💡"
-                link="/post-problem"
-                linkTitle="Post a Problem"
-            />
-        );
-    };
-    return (
-        <div className="mt-10 flex w-full flex-col gap-6">
-            {questions.length > 0
-                ? questions.map((question) => (
-                      <QuestionCard
-                          key={question._id}
-                          _id={question._id}
-                          clerkId={clerkId}
-                          title={question.title}
-                          skills={question.skills}
-                          author={question.author}
-                          upvotes={question.upvotes}
-                          views={question.views}
-                          answers={question.answers}
-                          createdAt={new Date(question.createdAt)}
-                      />
-                  ))
-                : renderNoResult()}
-        </div>
-    );
+		return (
+			<NoResult
+				title="No Questions Found"
+				description="Be the first to break the silence! 🚀 Post a Problem and kickstart the discussion. Your query could be the next big thing others learn from. Get involved! 💡"
+				link="/post-problem"
+				linkTitle="Post a Problem"
+			/>
+		);
+	};
+	return (
+		<div className="mt-10 flex w-full flex-col gap-6">
+			{questions.length > 0
+				? questions.map((question) => (
+						<QuestionCard
+							key={question._id}
+							_id={question._id}
+							clerkId={clerkId}
+							title={question.title}
+							skills={question.skills}
+							author={question.author}
+							upvotes={question.upvotes}
+							views={question.views}
+							answers={question.answers}
+							createdAt={new Date(question.createdAt)}
+						/>
+					))
+				: renderNoResult()}
+		</div>
+	);
 };
 
 export default QuestionsContainer;

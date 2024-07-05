43eb54814f2d9b157aa350ca6fa2e9525c1e319f
AliffiaRosita <aliffia.rosita@gmail.com>
2024-07-05 20:54:29 +0800

    fix middleware after auth condition

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,40 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ["/home", "/api/webhook/clerk", "/api/rapidapi"],
-    ignoredRoutes: [
-        "/terms-of-service",
-        "/privacy-policy",
-        "/api/webhook/clerk",
-        "/api/openai",
-        "/api/rapidapi",
-    ],
-    afterAuth(auth, req, evt) {
-        if (!auth.userId && !auth.isPublicRoute) {
-            return redirectToSignIn({ returnBackUrl: req.url });
-        }
-    },
+	publicRoutes: [
+		"/home",
+		"/api/webhook/clerk",
+		"/api/rapidapi",
+		"/question/:id",
+		"/skills",
+		"/skills/:id",
+		"/profile/:id",
+		"/community",
+		"/jobs",
+		"/post-problem",
+		"/onboarding",
+		"/message",
+		"/call/:id",
+	],
+	ignoredRoutes: [
+		"/terms-of-service",
+		"/privacy-policy",
+		"/api/webhook/clerk",
+		"/api/openai",
+		"/api/rapidapi",
+	],
+	afterAuth(auth, req, evt) {
+		const url = req.url;
+
+		const parts = url.split("/");
+
+		const lastPart = parts[parts.length - 1];
+		if (!auth.userId && lastPart !== "home") {
+			return redirectToSignIn({ returnBackUrl: req.url });
+		}
+	},
 });
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };

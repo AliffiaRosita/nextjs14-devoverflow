6f57d98b331a0acae11adb65e1cf5728f7f98bfb
AliffiaRosita <aliffia.rosita@gmail.com>
2024-07-05 20:30:32 +0800

    fix getStreamUserData params

--- hooks/useInitializeChatClient.ts
+++ hooks/useInitializeChatClient.ts
@@ -5,52 +5,56 @@ import { useEffect, useState } from "react";
 import { StreamChat } from "stream-chat";
 
 const useInitializeChatClient = () => {
-  const { user } = useUser();
-  const [chatClient, setChatClient] = useState<StreamChat | null>(null);
+	const { user } = useUser();
+	const [chatClient, setChatClient] = useState<StreamChat | null>(null);
 
-  useEffect(() => {
-    if (!user?.id) return;
+	useEffect(() => {
+		if (!user?.id) return;
 
-    const client = StreamChat.getInstance(
-      process.env.NEXT_PUBLIC_STREAM_API_KEY || ""
-    );
+		const client = StreamChat.getInstance(
+			process.env.NEXT_PUBLIC_STREAM_API_KEY || ""
+		);
 
-    const createStreamUser = async () => {
-      try {
-        const streamUser = await getStreamUserData(user.id);
+		const createStreamUser = async () => {
+			try {
+				const streamUser = await getStreamUserData({ userId: user.id });
 
-        if (!streamUser) throw new Error("Stream User is not exist");
+				if (!streamUser) throw new Error("Stream User is not exist");
 
-        client
-          .connectUser(
-            {
-              id: streamUser.id,
-              name: streamUser.name,
-              image: streamUser.image,
-            },
-            async () => {
-              return await streamTokenProvider(user.id);
-            }
-          )
-          .catch((error) => console.error("Failed to connect user", error))
-          .then(() => setChatClient(client));
-      } catch (error) {
-        console.log(error);
-      }
-    };
+				client
+					.connectUser(
+						{
+							id: streamUser.id,
+							name: streamUser.name,
+							image: streamUser.image,
+						},
+						async () => {
+							return await streamTokenProvider(user.id);
+						}
+					)
+					.catch((error) =>
+						console.error("Failed to connect user", error)
+					)
+					.then(() => setChatClient(client));
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-    createStreamUser();
+		createStreamUser();
 
-    return () => {
-      setChatClient(null);
-      client
-        .disconnectUser()
-        .catch((error) => console.error("Failed to disconnect user", error))
-        .then(() => console.log("Connection closed"));
-    };
-  }, [user]);
+		return () => {
+			setChatClient(null);
+			client
+				.disconnectUser()
+				.catch((error) =>
+					console.error("Failed to disconnect user", error)
+				)
+				.then(() => console.log("Connection closed"));
+		};
+	}, [user]);
 
-  return chatClient;
+	return chatClient;
 };
 
 export default useInitializeChatClient;
--- lib/actions/stream.actions.ts
+++ lib/actions/stream.actions.ts
@@ -7,33 +7,37 @@ const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const STREAM_API_SECRET = process.env.NEXT_STREAM_SECRET_KEY;
 
 export const streamTokenProvider = async (userId: string) => {
-  try {
-    const streamUser = await getStreamUserData(userId);
-    if (!streamUser) throw new Error("Stream User is not exist");
-    if (!STREAM_API_KEY) throw new Error("Stream API key secret is missing");
-    if (!STREAM_API_SECRET) throw new Error("Stream API secret is missing");
+	try {
+		const streamUser = await getStreamUserData({ userId });
+		if (!streamUser) throw new Error("Stream User is not exist");
+		if (!STREAM_API_KEY)
+			throw new Error("Stream API key secret is missing");
+		if (!STREAM_API_SECRET) throw new Error("Stream API secret is missing");
 
-    const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
+		const streamClient = new StreamClient(
+			STREAM_API_KEY,
+			STREAM_API_SECRET
+		);
 
-    await streamClient.upsertUsers({
-      users: {
-        [streamUser.id]: streamUser,
-      },
-    });
+		await streamClient.upsertUsers({
+			users: {
+				[streamUser.id]: streamUser,
+			},
+		});
 
-    const streamToken = streamClient.createToken(userId);
+		const streamToken = streamClient.createToken(userId);
 
-    await updateUser({
-      clerkId: userId,
-      updateData: {
-        streamToken,
-      },
-      path: "",
-    });
+		await updateUser({
+			clerkId: userId,
+			updateData: {
+				streamToken,
+			},
+			path: "",
+		});
 
-    return streamToken;
-  } catch (error) {
-    console.error(error);
-    throw Error("Failed to get token");
-  }
+		return streamToken;
+	} catch (error) {
+		console.error(error);
+		throw Error("Failed to get token");
+	}
 };

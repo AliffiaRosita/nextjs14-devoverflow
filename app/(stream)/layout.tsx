6d7516b5573c519f8aff4c4a0107e18f3717cbe7
Nur Hakim <nur10hkm@gmail.com>
2024-07-05 15:20:12 +0800

    Fix: redirect non boarded user on stream pages

--- app/(stream)/layout.tsx
+++ app/(stream)/layout.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/shared/navbar/Navbar";
 
+import { getUserById } from "@/lib/actions/user.action";
+
 import StreamVideoProvider from "@/context/StreamClientProvider";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const mongoUser = await getUserById({ userId });
+
+  if (!mongoUser?.onboarded) redirect("/onboarding");
+
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />

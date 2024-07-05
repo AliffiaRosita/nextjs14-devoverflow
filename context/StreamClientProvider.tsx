fd4e0326a290fa3896ef18a1fafa91c09804ae20
AliffiaRosita <aliffia.rosita@gmail.com>
2024-07-05 19:21:56 +0800

    change clerk hook

--- context/StreamClientProvider.tsx
+++ context/StreamClientProvider.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
-import { useUser } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 import { streamTokenProvider } from "@/lib/actions/stream.actions";
 import { getStreamUserData } from "@/lib/actions/user.action";
@@ -12,43 +12,43 @@ import Loader from "@/components/shared/Loader";
 const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
-  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-  const { user, isLoaded } = useUser();
+	const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+	const { userId, isLoaded } = useAuth();
 
-  useEffect(() => {
-    const createStreamUser = async () => {
-      try {
-        if (!isLoaded || !user) return;
-        if (!API_KEY) throw new Error("Stream API key is missing");
+	useEffect(() => {
+		const createStreamUser = async () => {
+			try {
+				if (!isLoaded || !userId) return;
+				if (!API_KEY) throw new Error("Stream API key is missing");
 
-        const streamUser = await getStreamUserData(user.id);
+				const streamUser = await getStreamUserData(userId);
+				console.log("streamUser :", streamUser);
+				if (!streamUser) throw new Error("Stream User is not exist");
 
-        if (!streamUser) throw new Error("Stream User is not exist");
+				const client = new StreamVideoClient({
+					apiKey: API_KEY,
+					user: {
+						id: streamUser.id,
+						name: streamUser.name,
+						image: streamUser.image,
+					},
+					tokenProvider: async () => {
+						return await streamTokenProvider(userId);
+					},
+				});
 
-        const client = new StreamVideoClient({
-          apiKey: API_KEY,
-          user: {
-            id: streamUser.id,
-            name: streamUser.name,
-            image: streamUser.image,
-          },
-          tokenProvider: async () => {
-            return await streamTokenProvider(user.id);
-          },
-        });
+				setVideoClient(client);
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-        setVideoClient(client);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+		createStreamUser();
+	}, [userId, isLoaded]);
 
-    createStreamUser();
-  }, [user, isLoaded]);
+	if (!videoClient) return <Loader />;
 
-  if (!videoClient) return <Loader />;
-
-  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
+	return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;

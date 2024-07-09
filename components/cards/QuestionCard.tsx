0c0c2743caab9d1dc1a8e351dfedf5ba48dce046
AliffiaRosita <aliffia.rosita@gmail.com>
2024-07-09 16:19:06 +0800

    fix: add mark on question card

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -18,6 +18,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { QuestionProps } from "@/types";
 import { useRouter } from "next/navigation";
+import { Badge } from "../ui/badge";
 
 const QuestionCard = ({
 	_id,
@@ -29,6 +30,7 @@ const QuestionCard = ({
 	answers,
 	createdAt,
 	clerkId,
+	mark,
 }: QuestionProps) => {
 	const isUserAuthor = clerkId && clerkId === author.clerkId;
 
@@ -38,6 +40,11 @@ const QuestionCard = ({
 		<div className="card-wrapper rounded-[10px] p-9 sm:px-11">
 			<div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
 				<div>
+					<Badge
+						className={`subtle-medium mb-5 ${mark === "solved" ? "bg-emerald-500 text-white" : "background-light800_dark300 text-light400_light500"}  rounded-md border-none px-4 py-2 uppercase`}
+					>
+						{mark}
+					</Badge>
 					<span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
 						{getTimestamp(createdAt)}
 					</span>

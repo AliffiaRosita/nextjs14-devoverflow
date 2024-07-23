654714b5f0fcf587aa77dc2eeb9ac31add342915
tabrez-88 <tabrez.upwork88@gmail.com>
2024-07-23 15:39:34 +0800

    Fix: change skill input component

--- lib/validations.ts
+++ lib/validations.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const QuestionValidation = z.object({
     title: z.string().min(5).max(130),
     explanation: z.string().max(1500),
-    skills: z.array(z.string().min(1)).min(1),
+    // skills: z.array(z.string().min(1)).min(1),
     mark: z.string(),
 });
 
@@ -22,5 +22,5 @@ export const ProfileValidation = z.object({
     zoom: z.union([z.string().min(5).max(50), z.literal("")]),
     skype: z.union([z.string().min(5).max(50), z.literal("")]),
     teams: z.union([z.string().min(5).max(50), z.literal("")]),
-    skills: z.array(z.string().min(1)).min(1),
+    // skills: z.array(z.string().min(1)).min(1),
 });

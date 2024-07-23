4a8fd384d789a267b49067c7c0be79eee8b45ea1
tabrez-88 <tabrez.upwork88@gmail.com>
2024-07-23 16:10:45 +0800

    fix : skill input on edit question

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import React, { useRef, useState } from "react";
-import { useRouter, usePathname } from "next/navigation";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Editor } from "@tinymce/tinymce-react";
+import React, { useRef, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Editor } from '@tinymce/tinymce-react';
 
 import {
     Form,
@@ -15,18 +15,18 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { toast } from "@/components/ui/use-toast";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
 
-import { useTheme } from "@/context/ThemeProvider";
+import { useTheme } from '@/context/ThemeProvider';
 
-import { QuestionValidation } from "@/lib/validations";
-import { editQuestion, createQuestion } from "@/lib/actions/question.action";
+import { QuestionValidation } from '@/lib/validations';
+import { editQuestion, createQuestion } from '@/lib/actions/question.action';
 
-import CreatableSelect from "react-select/creatable";
-import { MultiValue } from "react-select";
+import CreatableSelect from 'react-select/creatable';
+import { MultiValue } from 'react-select';
 interface Props {
     type: string;
     mongoUserId: string;
@@ -46,33 +46,41 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
     const pathname = usePathname();
     const options = [
         {
-            label: "Solved",
-            value: "solved",
+            label: 'Solved',
+            value: 'solved',
         },
         {
-            label: "Unsolved",
-            value: "unsolved",
+            label: 'Unsolved',
+            value: 'unsolved',
         },
     ];
 
-    const parsedSkills = skills && JSON.parse(skills || "");
+    const parsedSkills = skills && JSON.parse(skills || '');
 
     const parsedQuestionDetails =
-        questionDetails && JSON.parse(questionDetails || "");
+        questionDetails && JSON.parse(questionDetails || '');
 
     const checkMark = options.find(
-        (option) => option.value === parsedQuestionDetails?.mark
+        option => option.value === parsedQuestionDetails?.mark,
     );
     const [optionValue, setOptionValue] = useState<Option | undefined>(
-        checkMark || options[1]
+        checkMark || options[1],
     );
+
+    const existingSkill = parsedQuestionDetails?.skills.map((item: any) => {
+        return {
+            value: item.name,
+            label: item.name,
+        };
+    });
+
     const [selectedSkillOption, setSelectedSkillOption] = useState<
         MultiValue<Option>
-    >([]);
+    >(existingSkill || []);
 
-    const [skillValidation, setSkillValidation] = useState<string>("");
+    const [skillValidation, setSkillValidation] = useState<string>('');
 
-    const skillOptions = parsedSkills.map((item: any) => {
+    const skillOptions = parsedSkills?.map((item: any) => {
         return {
             value: item.name,
             label: item.name,
@@ -82,8 +90,8 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
     const form = useForm<z.infer<typeof QuestionValidation>>({
         resolver: zodResolver(QuestionValidation),
         defaultValues: {
-            title: parsedQuestionDetails?.title || "",
-            explanation: parsedQuestionDetails?.content || "",
+            title: parsedQuestionDetails?.title || '',
+            explanation: parsedQuestionDetails?.content || '',
             // skills: groupedSkills || [],
             mark: parsedQuestionDetails?.mark || optionValue?.value,
         },
@@ -92,7 +100,7 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
     async function onSubmit(values: z.infer<typeof QuestionValidation>) {
         setIsSubmitting(true);
         try {
-            if (type === "Edit") {
+            if (type === 'Edit') {
                 await editQuestion({
                     questionId: parsedQuestionDetails._id,
                     title: values.title,
@@ -105,13 +113,13 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
 
                 toast({
                     title: `Question ${
-                        type === "Edit" ? "edited" : "posted"
+                        type === 'Edit' ? 'edited' : 'posted'
                     } successfully 🎉`,
-                    variant: "default",
+                    variant: 'default',
                 });
             } else {
                 if (selectedSkillOption.length === 0) {
-                    setSkillValidation("Add at least 1 skill");
+                    setSkillValidation('Add at least 1 skill');
                     setIsSubmitting(false);
                 } else {
                     const skills = selectedSkillOption.map((item: Option) => {
@@ -125,21 +133,21 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
                         path: pathname,
                     });
                     // navigate to home page
-                    router.push("/home");
+                    router.push('/home');
                     setIsSubmitting(false);
 
                     toast({
                         title: `Question ${
-                            type === "Edit" ? "edited" : "posted"
+                            type === 'Edit' ? 'edited' : 'posted'
                         } successfully 🎉`,
-                        variant: "default",
+                        variant: 'default',
                     });
                 }
             }
         } catch (error) {
             toast({
-                title: `Error ${type === "Edit" ? "editing" : "posting"} question ⚠️`,
-                variant: "destructive",
+                title: `Error ${type === 'Edit' ? 'editing' : 'posting'} question ⚠️`,
+                variant: 'destructive',
             });
 
             console.error(error);
@@ -148,16 +156,15 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
 
     const handleMarkChange = (event: any) => {
         setOptionValue(event.target.value);
-        form.setValue("mark", event.target.value);
+        form.setValue('mark', event.target.value);
     };
 
     return (
         <Form {...form}>
             <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="flex w-full flex-col gap-10"
-            >
-                {type === "Edit" && (
+                className="flex w-full flex-col gap-10">
+                {type === 'Edit' && (
                     <FormField
                         control={form.control}
                         name="mark"
@@ -172,13 +179,11 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
                                         className=" no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] rounded-md border px-2"
                                         id="dropdown"
                                         value={optionValue?.value}
-                                        onChange={handleMarkChange}
-                                    >
+                                        onChange={handleMarkChange}>
                                         {options.map((option, index) => (
                                             <option
                                                 key={index}
-                                                value={option.value}
-                                            >
+                                                value={option.value}>
                                                 {option.label}
                                             </option>
                                         ))}
@@ -233,45 +238,45 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
                                         editorRef.current = editor;
                                     }}
                                     onBlur={field.onBlur}
-                                    onEditorChange={(content) =>
+                                    onEditorChange={content =>
                                         field.onChange(content)
                                     }
                                     initialValue={
-                                        parsedQuestionDetails?.content || ""
+                                        parsedQuestionDetails?.content || ''
                                     }
                                     init={{
                                         height: 350,
                                         menubar: false,
                                         plugins: [
-                                            "advlist",
-                                            "autolink",
-                                            "lists",
-                                            "link",
-                                            "image",
-                                            "charmap",
-                                            "preview",
-                                            "anchor",
-                                            "searchreplace",
-                                            "visualblocks",
-                                            "codesample",
-                                            "fullscreen",
-                                            "insertdatetime",
-                                            "media",
-                                            "table",
-                                            "wordcount",
+                                            'advlist',
+                                            'autolink',
+                                            'lists',
+                                            'link',
+                                            'image',
+                                            'charmap',
+                                            'preview',
+                                            'anchor',
+                                            'searchreplace',
+                                            'visualblocks',
+                                            'codesample',
+                                            'fullscreen',
+                                            'insertdatetime',
+                                            'media',
+                                            'table',
+                                            'wordcount',
                                         ],
                                         toolbar:
-                                            "undo redo | " +
-                                            "codesample | bold italic forecolor | alignleft aligncenter | link image |" +
-                                            "alignright alignjustify | bullist numlist outdent indent",
+                                            'undo redo | ' +
+                                            'codesample | bold italic forecolor | alignleft aligncenter | link image |' +
+                                            'alignright alignjustify | bullist numlist outdent indent',
                                         content_style:
-                                            "body { font-family:Inter; font-size:16px }",
+                                            'body { font-family:Inter; font-size:16px }',
                                         skin:
-                                            mode === "dark"
-                                                ? "oxide-dark"
-                                                : "oxide",
+                                            mode === 'dark'
+                                                ? 'oxide-dark'
+                                                : 'oxide',
                                         content_css:
-                                            mode === "dark" ? "dark" : "light",
+                                            mode === 'dark' ? 'dark' : 'light',
                                     }}
                                 />
                             </FormControl>
@@ -293,7 +298,8 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
                             defaultValue={selectedSkillOption}
                             onChange={setSelectedSkillOption}
                             isMulti
-                            placeholder={"Select skill"}
+                            isDisabled={type === 'Edit' ? true : false}
+                            placeholder={'Select skill'}
                             options={skillOptions}
                         />
                     </FormControl>
@@ -309,15 +315,14 @@ const Question = ({ type, mongoUserId, questionDetails, skills }: Props) => {
                 <Button
                     type="submit"
                     className="primary-gradient w-fit !text-light-900"
-                    disabled={isSubmitting}
-                >
+                    disabled={isSubmitting}>
                     {isSubmitting ? (
-                        <>{type === "Edit" ? "Editing..." : "Posting..."}</>
+                        <>{type === 'Edit' ? 'Editing...' : 'Posting...'}</>
                     ) : (
                         <>
-                            {type === "Edit"
-                                ? "Edit Question"
-                                : "Post a Problem"}
+                            {type === 'Edit'
+                                ? 'Edit Question'
+                                : 'Post a Problem'}
                         </>
                     )}
                 </Button>

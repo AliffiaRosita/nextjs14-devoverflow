adb662a376efce5d5bd5713ad0a7a91a032980e2
pius706975 <piusrestiantoro2@gmail.com>
2024-07-26 12:02:52 +0800

    SGP-39(fix) Banner created in home page

--- app/(root)/(home)/home/page.tsx
+++ app/(root)/(home)/home/page.tsx
@@ -1,106 +1,114 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import { auth } from "@clerk/nextjs/server";
+import { auth } from '@clerk/nextjs/server';
 
-import { Button } from "@/components/ui/button";
-import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import Filter from "@/components/shared/Filter";
-import Pagination from "@/components/shared/Pagination";
-import HomeFilters from "@/components/shared/Filters";
-import QuestionsContainer from "@/components/shared/QuestionsContainer";
+import { Button } from '@/components/ui/button';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import Filter from '@/components/shared/Filter';
+import Pagination from '@/components/shared/Pagination';
+import HomeFilters from '@/components/shared/Filters';
+import QuestionsContainer from '@/components/shared/QuestionsContainer';
 
-import { HomePageFilters } from "@/constants/filters";
+import { HomePageFilters } from '@/constants/filters';
 
-import type { SearchParamsProps } from "@/types";
-import type { Metadata } from "next";
+import type { SearchParamsProps } from '@/types';
+import type { Metadata } from 'next';
 import {
-	getQuestions,
-	getRecommendedQuestions,
-} from "@/lib/actions/question.action";
-import { identifyKnockUser } from "@/lib/actions/knock.action";
-import { getUserById } from "@/lib/actions/user.action";
+    getQuestions,
+    getRecommendedQuestions,
+} from '@/lib/actions/question.action';
+import { identifyKnockUser } from '@/lib/actions/knock.action';
+import { getUserById } from '@/lib/actions/user.action';
+import { ImageSwiper } from '@/components/banner/banner';
 
 export const metadata: Metadata = {
-	title: "Home — TheSkillGuru",
+    title: 'Home — TheSkillGuru',
 };
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-	const { userId: clerkId } = auth();
-	let mongoUserId;
+    const { userId: clerkId } = auth();
+    let mongoUserId;
 
-	if (clerkId) {
-		await identifyKnockUser(clerkId)
-		const mongoUser = await getUserById({ userId: clerkId });
-		mongoUserId = mongoUser ? mongoUser._id : null;
-	}
+    if (clerkId) {
+        await identifyKnockUser(clerkId);
+        const mongoUser = await getUserById({ userId: clerkId });
+        mongoUserId = mongoUser ? mongoUser._id : null;
+    }
 
-	let result;
+    let result;
 
-	if (searchParams?.filter === "recommended") {
-		if (clerkId) {
-			result = await getRecommendedQuestions({
-				userId: clerkId,
-				searchQuery: searchParams.q,
-				page: searchParams.page ? +searchParams.page : 1,
-			});
-		} else {
-			result = {
-				questions: [],
-				isNext: false,
-			};
-		}
-	} else {
-		result = await getQuestions({
-			searchQuery: searchParams.q,
-			filter: searchParams.filter,
-			page: searchParams.page ? +searchParams.page : 1,
-			clerkId: clerkId || "",
-		});
-	}
+    if (searchParams?.filter === 'recommended') {
+        if (clerkId) {
+            result = await getRecommendedQuestions({
+                userId: clerkId,
+                searchQuery: searchParams.q,
+                page: searchParams.page ? +searchParams.page : 1,
+            });
+        } else {
+            result = {
+                questions: [],
+                isNext: false,
+            };
+        }
+    } else {
+        result = await getQuestions({
+            searchQuery: searchParams.q,
+            filter: searchParams.filter,
+            page: searchParams.page ? +searchParams.page : 1,
+            clerkId: clerkId || '',
+        });
+    }
 
-	const questions = JSON.parse(JSON.stringify(result.questions));
+    const questions = JSON.parse(JSON.stringify(result.questions));
 
-	return (
-		<>
-			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-				<h1 className="h1-bold text-dark100_light900">All Problems</h1>
+    return (
+        <>
+            <div className="mb-10 w-full">
+                <ImageSwiper />
+            </div>
 
-				<Link
-					prefetch={false}
-					href="/post-problem"
-					className="flex justify-end max-sm:w-full"
-				>
-					<Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-						Post a Problem
-					</Button>
-				</Link>
-			</div>
+            <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+                <h1 className="h1-bold text-dark100_light900">All Problems</h1>
 
-			<div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-				<LocalSearchbar
-					route="/home"
-					iconPosition="left"
-					imgSrc="/assets/icons/search.svg"
-					placeholder="Search for problems"
-					otherClasses="flex-1"
-				/>
+                <Link
+                    prefetch={false}
+                    href="/post-problem"
+                    className="flex justify-end max-sm:w-full">
+                    <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+                        Post a Problem
+                    </Button>
+                </Link>
+            </div>
 
-				<Filter
-					filters={HomePageFilters}
-					otherClasses="min-h-[56px] sm:min-w-[170px]"
-					containerClasses=" max-md:flex"
-				/>
-			</div>
+            <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+                <LocalSearchbar
+                    route="/home"
+                    iconPosition="left"
+                    imgSrc="/assets/icons/search.svg"
+                    placeholder="Search for problems"
+                    otherClasses="flex-1"
+                />
 
-			<HomeFilters filters={HomePageFilters} />
-			<QuestionsContainer questions={questions} clerkId={clerkId} mongoUserId={mongoUserId} />
+                <Filter
+                    filters={HomePageFilters}
+                    otherClasses="min-h-[56px] sm:min-w-[170px]"
+                    containerClasses=" max-md:flex"
+                />
+            </div>
 
-			<div className="mt-10">
-				<Pagination
-					pageNumber={searchParams?.page ? +searchParams.page : 1}
-					isNext={result.isNext}
-				/>
-			</div>
-		</>
-	);
+            <HomeFilters filters={HomePageFilters} />
+            <QuestionsContainer
+                questions={questions}
+                clerkId={clerkId}
+                mongoUserId={mongoUserId}
+            />
+
+            <div className="mt-10">
+                <Pagination
+                    pageNumber={searchParams?.page ? +searchParams.page : 1}
+                    isNext={result.isNext}
+                />
+            </div>
+        </>
+    );
 }
---
+++ components/banner/banner.tsx
@@ -0,0 +1,87 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+    Navigation,
+    Pagination,
+    Scrollbar,
+    A11y,
+    Autoplay,
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import banner from './banner.json';
+
+const banners = [
+    { img: banner.img1 },
+    { img: banner.img2 },
+    { img: banner.img3 },
+];
+
+export const ImageSwiper = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        handleResize(); // Initial check
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        const paginationBullets = document.querySelectorAll(
+            '.swiper-pagination-bullet',
+        );
+        paginationBullets.forEach(bullet => {
+            (bullet as HTMLElement).style.backgroundColor = '#0490da';
+        });
+
+        const activeBullet = document.querySelector(
+            '.swiper-pagination-bullet-active',
+        );
+        if (activeBullet) {
+            (activeBullet as HTMLElement).style.backgroundColor = '#a5f3fc';
+        }
+    }, []);
+
+    // Mobile device isn't fixed yet
+    return (
+        <div>
+            {!isMobile && (
+                <div className="h-80 w-full rounded-xl object-cover">
+                    <Swiper
+                        modules={[
+                            Navigation,
+                            Pagination,
+                            Scrollbar,
+                            A11y,
+                            Autoplay,
+                        ]}
+                        spaceBetween={10}
+                        slidesPerView={1}
+                        autoplay={{ delay: 3000 }}
+                        loop={true}
+                        pagination={{ clickable: true }}>
+                        {banners.map((item, index) => (
+                            <SwiperSlide key={index + 1}>
+                                <img
+                                    className="h-80 w-full rounded-xl"
+                                    src={item.img}
+                                    alt={`Image ${index + 1}`}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            )}
+        </div>
+    );
+};
